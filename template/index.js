7651f619741e3bc3342a2cdb6d132ee02a25d69a
arthurrafel <arthur.r.candido@edu.senai.br>
2026-09-03 16:00:20 -0300

    feat adicionar desafio final

--- template/index.js
+++ template/index.js
@@ -11,81 +11,82 @@ let playlist = [
     "Paranoid"
 ];
 
-alert ("Playlist atual: " + playlist.join(", "));
-alert ("Opções:");
-alert (" 1 - Pesquisar música")
-alert (" 2 - Criar seleção")
-alert (" 3 - Remover música")
-alert (" 4 - Inserir música")
-alert (" 5 - Ordenar A-Z")
-alert (" 6 - Inverter playlist")
-alert (" 7 - Exibir playlist")
-alert (" 8 - Exibir playlist completa")
-alert (" 0 - Sair")
+console.log ("Playlist atual: " + playlist.join(", "));
+console.log ("Opções:");
+console.log (" 1 - Pesquisar música")
+console.log (" 2 - Criar seleção")
+console.log (" 3 - Remover música")
+console.log (" 4 - Inserir música")
+console.log (" 5 - Ordenar A-Z")
+console.log (" 6 - Inverter playlist")
+console.log (" 7 - Exibir playlist")
+console.log (" 8 - Exibir playlist completa")
+console.log (" 0 - Sair")
 
-let opcao = 1
+let opcao = "4";
 
 
-if (opcao = 1) {alert ("Qual músicsa você quer ouvir? Digiet abaixo..");
+if (opcao == "1") {
+    console.log ("Qual músicsa você quer ouvir? Digiet abaixo..");
 
 if (playlist.includes("Fear of the Dark")) {
-    alert ("Música encontrada! Na posição: " + playlist.indexOf("Fear of the Dark"));
+    console.log ("Música encontrada! Na posição: " + playlist.indexOf("Fear of the Dark"));
 } 
 
 else {
-    alert ("Música não encontrada!");
+    console.log ("Música não encontrada!");
 }
 }
 
-else if (opcao = 2) { 
-    alert ("Informe duas posições da playlist para criar uma nova playlist:")
+else if (opcao == "2") { 
+    console.log ("Informe duas posições da playlist para criar uma nova playlist:")
 
         let posicoes = playlist.slice(1, 4);
 
-    alert ("Nova playlist criada: " + posicoes);
+    console.log ("Nova playlist criada: " + posicoes);
 }
 
-else if (opcao = 3) {
-    alert ("Vamos remover uma música da playlist. Digite a música que deseja remover: ");
+else if (opcao == "3") {
+    console.log ("Vamos remover uma música da playlist. Digite a música que deseja remover: ");
 
         playlist.splice("Breaking the Law", 1);
     
-    alert ("Playlist atualizada: " + playlist);
+    console.log ("Playlist atualizada: " + playlist);
 }
 
-else if (opcao = 4) {
-        alert ("vamos adicionar uma música. Digite o nome da música que deseja adicionar: ");
+else if (opcao == "4") {
+        console.log ("vamos adicionar uma música. Digite o nome da música que deseja adicionar: ");
 
     let novamusica = "Billie jean";
 
-        alert ("Informe a posição que deseja adicionar a música: ");
+        console.log ("Informe a posição que deseja adicionar a música: ");
 
     playlist.splice(2, 0, novamusica);
 
-        alert ("Playlist atualizada: " + playlist);
+        console.log ("Playlist atualizada: " + playlist);
 }
 
-else if (opcao = 5) {
-    alert ("Playlist em ordem alfabética:");
-    alert (playlist.sort());
+else if (opcao == "5") {
+    console.log ("Playlist em ordem alfabética:");
+    console.log (playlist.sort());
 }
 
-else if (opcao = 6) {
-    alert ("Playlist invertida:");
-    alert (playlist.reverse());
+else if (opcao == "6") {
+    console.log ("Playlist invertida:");
+    console.log (playlist.reverse());
 }
 
-else if (opcao = 7) {
-    alert ("transformando em string:");
-    alert (playlist.join("|"));
+else if (opcao == "7") {
+    console.log ("transformando em string:");
+    console.log (playlist.join("|"));
 }
 
-else if (opcao = 8) {
-    alert ("==== Minha Playlist ====");
-    alert (playlist.join(", "));
+else if (opcao == "8") {
+    console.log ("==== Minha Playlist ====");
+    console.log (playlist.join(", "));
 }
 
 else {
-    alert ("Você saiu")
+    console.log ("Você saiu")
 }
 
